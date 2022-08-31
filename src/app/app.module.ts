736a284c16ab80daa613fc45cf44625e19f396c4
FARHAN IFTIKHAR <farhaniftikhar@stellatechnology.com>
2022-08-31 12:15:29 +0500

    IGTI FRONTEND

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavLogComponent } from './nav-log/nav-log.component';
+import { HeaderComponent } from './header/header.component';
+import {InputTextModule} from "primeng/inputtext";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {TableModule} from "primeng/table";
+import { NavNotesAndObservationComponent } from './nav-notes-and-observation/nav-notes-and-observation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavLogComponent,
+    HeaderComponent,
+    NavNotesAndObservationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    RouterModule,
+    AppRoutingModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
